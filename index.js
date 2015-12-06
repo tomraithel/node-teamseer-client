ea9249e141acc02280d206337341ca57283bf78d
Tom Raithel <t.raithel@googlemail.com>
2015-12-06 19:40:50 +0100

    Export adapters, fixed response for empty soap

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 'use strict';
 
 require("babel-register");
-module.exports = require('./lib/client');
+var Client = require('./lib/client');
+var SoapAdapter = require('./lib/adapters/soap');
+
+module.exports = {
+	Client: Client,
+	adapters: {
+		Soap: SoapAdapter
+	}
+};
