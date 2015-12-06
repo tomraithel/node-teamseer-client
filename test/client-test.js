2a67061767efeb21c2d943f3c733934f7690f54e
Tom Raithel <t.raithel@googlemail.com>
2015-12-06 19:04:36 +0100

    Added more tests and implementation for client

--- test/client-test.js
+++ test/client-test.js
@@ -1,9 +1,33 @@
-var expect = require('chai').expect;
+var chai = require('chai');
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var chaiAsPromised = require("chai-as-promised");
+
+var expect = chai.expect;
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+
+var Rx = require('rx');
+
 var Client = require('../lib/client');
+var SoapAdapter = require('../lib/adapters/soap');
 
 
 
 describe('Client', function() {
+
+	var client;
+	var conf;
+
+	beforeEach(function() {
+		conf = {
+			companyId: 123,
+			companyKey: 'mykey',
+			apiVersion: '1_0_1'
+		};
+		client = new Client(conf, {});
+	});
+
 
 	describe('#constructor', function() {
 		it('should be defined', function () {
@@ -11,10 +35,73 @@ describe('Client', function() {
 		});
 
 		it('should populate the getters', function () {
-			var client = new Client(123, 'mykey', '1_0_1');
 			expect(client.getCompanyId()).to.equal(123);
 			expect(client.getCompanyKey()).to.equal('mykey');
 			expect(client.getApiVersion()).to.equal('1_0_1');
+		});
+	});
+
+	describe('#isAuthenticated', function() {
+		it('should be false after initialization', function () {
+			expect(client.isAuthenticated()).to.equal(false);
+		});
+
+		it('should be true after auth has been set', function () {
+			client.setAuth(new Date());
+			expect(client.isAuthenticated()).to.equal(true);
+		});
+
+		it('should be true after auth has been set and some time has passed', function () {
+			var expiredTime = new Date().getTime() - 29 * 60 * 1000;
+			client.setAuth(new Date(expiredTime));
+			expect(client.isAuthenticated()).to.equal(true);
+		});
+
+		it('should be false after last auth has been expired', function () {
+			var expiredTime = new Date().getTime() - 40 * 60 * 1000;
+			client.setAuth(new Date(expiredTime));
+			expect(client.isAuthenticated()).to.equal(false);
+		});
+	});
+
+	describe('#getRecordsByUser', function() {
+		var client;
+		var mock;
+
+		beforeEach(function() {
+			mock = {
+				authenticate: sinon.spy(function() {
+					return Rx.Observable.just({
+						value: true,
+						cookie: 'PHPSESSID=123456'
+					});
+				}),
+				getRecordsFor: sinon.spy(function() {
+					return Rx.Observable.just(['2015-10-01', '2015-10-02', '2015-10-03']);
+				})
+			};
+			client = new Client(conf, mock);
+		});
+
+		it('should call authenticate on the adapter if not authenticated', function () {
+			client.getRecordsByUser();
+			expect(mock.authenticate).to.have.been.calledWith(conf);
+		});
+
+		it('should not call authenticate on the adapter if authenticated', function () {
+			client.setAuth(new Date());
+			client.getRecordsByUser();
+			expect(mock.authenticate).to.not.have.been.called;
+		});
+
+		it('should call the getRecordsFor method on the adapter', function (done) {
+			var promise = client.getRecordsByUser('tom', '2015-10-01', '2015-12-01');
+			expect(mock.getRecordsFor).to.have.been.calledWith({
+				userIdentifier: 'tom',
+				fromDate: '2015-10-01',
+				toDate: '2015-12-01'
+			});
+			expect(promise).to.eventually.deep.equal(['2015-10-01', '2015-10-02', '2015-10-03']).notify(done);
 		});
 	});
 });
--- test/soap-adapter-test.js
+++ test/soap-adapter-test.js
@@ -3,14 +3,15 @@ var SoapAdapter = require('../lib/adapters/soap');
 
 require('dotenv').load();
 
-//var testSubscription = function(source) {
-//
-//};
-
 var validWsdl = 'https://www.teamseer.com/services/soap/coreapi/1_0_1/teamseer_core_api.wsdl';
 
-
 describe('SoapApiInterface', function() {
+
+	//return; // Disabled
+
+	// Give us a little bit more time - mocha has a default of 2000ms before
+	// tests exceed timeout
+	this.timeout(8000);
 
 	describe('dotenv', function() {
 		it('.env should be loaded', function () {
@@ -49,7 +50,8 @@ describe('SoapApiInterface', function() {
 			client.getSource().subscribe(
 				function (x) {
 					expect(x).to.be.a('object');
-					expect(x.authenticate).to.be.a('function');
+					expect(x.client).to.be.a('object');
+					expect(x.client.authenticate).to.be.a('function');
 					done();
 				},
 				function (e) {},
@@ -59,7 +61,9 @@ describe('SoapApiInterface', function() {
 
 
 	describe('API Methods', function() {
+
 		var client,
+			cookie = 'PHPSESSID=ac0f78afe450b0cc55072a142e1c8ed2',
 			companyId = process.env.COMPANY_ID,
 			companyKey = process.env.COMPANY_KEY,
 			apiVersion = '1_0_1';
@@ -70,9 +74,46 @@ describe('SoapApiInterface', function() {
 
 		describe('#authenticate', function() {
 			it('should return true on login', function (done) {
-				client.authenticate(companyId, companyKey, apiVersion).subscribe(
-					function (x) {
-						expect(x).to.equal(true);
+				client.authenticate({
+					companyId: companyId,
+					companyKey: companyKey,
+					apiVersion: apiVersion
+				}).subscribe(
+					function (data) {
+						expect(data.value).to.equal(true);
+						expect(data.cookie).to.be.a('string');
+
+						cookie = data.cookie;
+						done();
+					}
+				);
+			});
+		});
+
+		describe('#getActiveUsers', function() {
+			it('should return a list of active users', function (done) {
+				client.getActiveUsers(null, {
+					'Cookie': cookie
+				}).subscribe(
+					function (data) {
+						expect(data).to.be.a('array');
+						done();
+					}
+				);
+			});
+		});
+
+		describe('#getRecordsFor', function() {
+			it('should give some records for a user', function (done) {
+				client.getRecordsFor({
+					userIdentifier: 'thomas.raithel@aoe.com',
+					fromDate: '2015-01-01',
+					toDate: '2015-12-01'
+				}, {
+					'Cookie': cookie
+				}).subscribe(
+					function (data) {
+						expect(data).to.be.a('array');
 						done();
 					}
 				);
